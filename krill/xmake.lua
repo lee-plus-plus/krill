595dd122cbe1969e7acd0820083eac349288d510
Zherui Yang <yangzherui2001@foxmail.com>
2023-01-19 10:05:08 +0800

    add xmake build system support.

---
+++ krill/xmake.lua
@@ -0,0 +1,8 @@
+add_requires('spdlog', {configs={fmt_external = true}})
+add_requires('magic_enum')
+
+target('krill')
+  set_kind('static')
+  add_includedirs('include', {public = true})
+  add_files('src/**.cpp')
+  add_packages('spdlog', 'magic_enum', {public = true})
