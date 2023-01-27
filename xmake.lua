fd62a0046a81f8071d599856e2ebffa6f7a60e27
Zherui Yang <yangzherui2001@foxmail.com>
2023-01-27 19:20:37 +0800

    Update: Add cxxopts, support cxx17 instead of cxx20

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,9 @@
 set_project('krill')
-set_languages('cxx20')
+set_languages('cxx17')
 set_warnings('allextra') -- -Wall
 add_rules("mode.debug", "mode.release")
 
 includes(
-  'krill', 'test', 'standalone'
+  '3rdparty', 'krill', 'test', 'standalone'
 )
 
